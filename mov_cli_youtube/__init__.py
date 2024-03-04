e28746955dd43af2440104c1e916ecd6f13f5399
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-03-04 22:25:18 +0000

    feat: add default scraper as overwise an upcoming mov-cli change will break scraper selection.

--- mov_cli_youtube/__init__.py
+++ mov_cli_youtube/__init__.py
@@ -1,8 +1,15 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from mov_cli.plugins import PluginHookData
+
 from .scraper import *
 
-plugin = {
+plugin: PluginHookData = {
+    "version": 1, 
     "scrapers": {
-        "youtube": YouTubeScraper
+        "DEFAULT": YouTubeScraper
     }
 }
 
