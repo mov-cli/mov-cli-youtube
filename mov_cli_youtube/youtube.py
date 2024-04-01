e8c6f32afbc731c887518b26e474adeb0f0cd5f8
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-04-01 14:57:04 +0000

    feat: swap to yt-dlp for searching

--- mov_cli_youtube/youtube.py
+++ mov_cli_youtube/youtube.py
@@ -9,10 +9,12 @@ if TYPE_CHECKING:
 
 import os
 import sys
+import json
+import yt_dlp
 from pytube import YouTube, Search
 
-from mov_cli import utils
 from mov_cli.scraper import Scraper
+from mov_cli.utils import EpisodeSelector
 from mov_cli import Series, Movie, Metadata, MetadataType
 
 __all__ = ("YouTubeScraper",)
@@ -21,39 +23,66 @@ class YouTubeScraper(Scraper):
     def __init__(self, config: Config, http_client: HTTPClient) -> None:
         super().__init__(config, http_client)
 
-    def search(self, query: str, limit: int = None) -> Generator[Metadata, Any, None]:
-        search_query = Search(query)
-        search_results: List[YouTube] = search_query.results
+    def search(self, query: str, limit: int = None, **kwargs) -> Generator[Metadata, Any, None]:
+        max_videos = 100 if limit is None else limit
 
-        max_videos = 20 if limit is None else limit
+        yt_options = {
+            "format": "best", 
+            "noplaylist":"True", 
+            "default_search": "ytsearch", 
+            "nocheckcertificate": True, 
+            "geo_bypass": True, 
+            "extract_flat": "in_playlist", 
+            "skip_download": True, 
+            "quiet": False if self.config.debug else True, 
+            "ignoreerrors": True, 
+            "match_filter": self.__yt_dlp_filter(**kwargs)
+        }
 
-        # suppress pytube's stupid errors from getting to the console and ruining fzf output.
-        sys.stderr = open(os.devnull, "w")
+        with yt_dlp.YoutubeDL(yt_options) as ydl:
+            info = ydl.extract_info(f"ytsearch{max_videos}:{query}", download = False)
 
-        for index, video in enumerate(search_results):
-            if (index + 1) == len(search_results) and not (index + 1) >= max_videos:
-                search_query.get_next_results()
+            for key in info["entries"]:
 
-            yield Metadata(
-                id = video.watch_url, 
-                title = f"{video.title} ~ {video.author}", 
-                type = MetadataType.MOVIE, 
-                year = str(video.publish_date.year)
-            )
+                yield Metadata(
+                    id = key["url"], 
+                    title = f"{key['title']} ~ {key['uploader']}", 
+                    type = MetadataType.MOVIE, 
+                    year = key["release_timestamp"]
+                )
 
-        # restore the console.
-        sys.stderr = sys.__stderr__
+        # # suppress pytube's stupid errors from getting to the console and ruining fzf output.
+        # sys.stderr = open(os.devnull, "w")
 
-    def scrape(self, metadata: Metadata, episode: Optional[utils.EpisodeSelector] = None, **kwargs: Dict[str, bool]) -> Series | Movie:
-        audio_only: bool = kwargs.get("audio", False)
+        # for index, video in enumerate(search_results):
+        #     if (index + 1) == len(search_results) and not (index + 1) >= max_videos:
+        #         search_query.get_next_results()
+
+        #     yield Metadata(
+        #         id = video.watch_url, 
+        #         title = f"{video.title} ~ {video.author}", 
+        #         type = MetadataType.MOVIE, 
+        #         year = str(video.publish_date.year)
+        #     )
+
+        # # restore the console.
+        # sys.stderr = sys.__stderr__
+
+    def scrape(
+        self, 
+        metadata: Metadata, 
+        episode: Optional[EpisodeSelector] = None, 
+        audio = False, 
+        **kwargs: Dict[str, bool]
+    ) -> Series | Movie:
 
         if episode is None:
-            episode = utils.EpisodeSelector()
+            episode = EpisodeSelector()
 
         watch_url = metadata.id
         video = YouTube(watch_url)
 
-        if audio_only:
+        if audio:
             url = video.streams.get_audio_only().url
         else:
             url = video.streams.get_highest_resolution().url
@@ -69,3 +98,13 @@ class YouTubeScraper(Scraper):
     def scrape_episodes(self, metadata: Metadata, **kwargs) -> Dict[None, int]:
         # Returning None as search does not return any metadata of type series.
         return {None: 1}
+
+    def __yt_dlp_filter(self, shorts: bool = False, **kwargs):
+
+        def filter(info, *, incomplete):
+            url = info.get("url")
+
+            if shorts is False and "/shorts" in url:
+                return "Video is a youtube short. Pass '--shorts' to the scraper to scrape for shorts."
+
+        return filter
