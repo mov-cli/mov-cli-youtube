1b14c4bbfca2e498c38c8e040ae89d0d7ebb36ad
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-04-15 20:11:15 +0100

    feat: ported over to mov-cli v4.3

--- mov_cli_youtube/youtube.py
+++ mov_cli_youtube/youtube.py
@@ -6,6 +6,7 @@ if TYPE_CHECKING:
 
     from mov_cli import Config
     from mov_cli.http_client import HTTPClient
+    from mov_cli.scraper import ScraperOptionsT
 
 import yt_dlp
 from pytube import YouTube
@@ -17,10 +18,10 @@ from mov_cli import Series, Movie, Metadata, MetadataType
 __all__ = ("YouTubeScraper",)
 
 class YouTubeScraper(Scraper):
-    def __init__(self, config: Config, http_client: HTTPClient) -> None:
-        super().__init__(config, http_client)
+    def __init__(self, config: Config, http_client: HTTPClient, options: Optional[ScraperOptionsT] = None) -> None:
+        super().__init__(config, http_client, options)
 
-    def search(self, query: str, limit: int = None, **kwargs) -> Generator[Metadata, Any, None]:
+    def search(self, query: str, limit: Optional[int] = None) -> Generator[Metadata, Any, None]:
         max_videos = 100 if limit is None else limit
 
         yt_options = {
@@ -33,7 +34,7 @@ class YouTubeScraper(Scraper):
             "skip_download": True, 
             "quiet": False if self.config.debug else True, 
             "ignoreerrors": True, 
-            "match_filter": self.__yt_dlp_filter(**kwargs)
+            "match_filter": self.__yt_dlp_filter(**self.options)
         }
 
         with yt_dlp.YoutubeDL(yt_options) as ydl:
@@ -51,18 +52,13 @@ class YouTubeScraper(Scraper):
     def scrape(
         self, 
         metadata: Metadata, 
-        episode: Optional[EpisodeSelector] = None, 
-        audio = False, 
-        **kwargs: Dict[str, bool]
+        _: EpisodeSelector
     ) -> Series | Movie:
-
-        if episode is None:
-            episode = EpisodeSelector()
 
         watch_url = metadata.id
         video = YouTube(watch_url)
 
-        if audio:
+        if self.options.get("audio", False):
             url = video.streams.get_audio_only().url
         else:
             url = video.streams.get_highest_resolution().url
@@ -75,7 +71,7 @@ class YouTubeScraper(Scraper):
             subtitles = None
         )
 
-    def scrape_episodes(self, metadata: Metadata, **kwargs) -> Dict[None, int]:
+    def scrape_episodes(self, _: Metadata) -> Dict[None, int]:
         # Returning None as search does not return any metadata of type series.
         return {None: 1}
 
@@ -85,6 +81,6 @@ class YouTubeScraper(Scraper):
             url = info.get("url")
 
             if shorts is False and "/shorts" in url:
-                return "Video is a youtube short. Pass '--shorts' to the scraper to scrape for shorts."
+                return "Video is a YouTube short. Pass '--shorts' to the scraper to scrape for them."
 
         return filter
